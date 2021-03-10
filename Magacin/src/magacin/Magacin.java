6b34abff7d4f9e4cf7231179bd245400f9e0fae5
DANKoOoO <danko.97@hotmail.com>
2021-03-10 12:24:50 +0100

    Implementirane metode u klasi Magacin.

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,25 +1,40 @@
 package magacin;
 
+import java.util.LinkedList;
+
 import interfejs.IMagacin;
 
 public class Magacin implements IMagacin {
-
+	
+	private LinkedList<Artikal> artikli = new LinkedList<Artikal>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikli.contains(artikal))
+			for(Artikal a:artikli)
+			if(a==artikal)
+				a.setKolicina(a.getKolicina()+1);
+		else
+			artikli.add(artikal);
 
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikli.contains(artikal))
+			for(Artikal a:artikli)
+			if(a==artikal)
+				a.setKolicina(a.getKolicina()-1);
 
 	}
 
 	@Override
 	public Artikal vratiArtikal(int sifra) {
-		// TODO Auto-generated method stub
-		return null;
+		Artikal artikal = null;
+		for(Artikal a: artikli)
+			if(a.getSifra() == sifra)
+				artikal = a;
+		return artikal;
 	}
 
 }
