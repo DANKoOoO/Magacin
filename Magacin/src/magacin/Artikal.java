0c952ffae1c19ba8be7782e78b7fe141df8cf74c
DANKoOoO <danko.97@hotmail.com>
2021-03-10 12:14:40 +0100

    Dodata je logicka kontrola za set metode

--- Magacin/src/magacin/Artikal.java
+++ Magacin/src/magacin/Artikal.java
@@ -9,24 +9,33 @@ public class Artikal {
 		return sifra;
 	}
 	public void setSifra(int sifra) {
+		if(sifra<0)
+			throw new RuntimeException("Sifra ne moze biti manja od 0");
+		
 		this.sifra = sifra;
 	}
 	public String getNaziv() {
 		return naziv;
 	}
 	public void setNaziv(String naziv) {
+		if(naziv==null)
+			throw new NullPointerException("Naziv ne moze biti null");
 		this.naziv = naziv;
 	}
 	public String getOpis() {
 		return opis;
 	}
 	public void setOpis(String opis) {
+		if(opis==null)
+			throw new NullPointerException("Opis ne moze biti null");
 		this.opis = opis;
 	}
 	public int getKolicina() {
 		return kolicina;
 	}
 	public void setKolicina(int kolicina) {
+		if(kolicina<=0)
+			throw new RuntimeException("Kolicina ne moze biti manja ili jednaka od 0");
 		this.kolicina = kolicina;
 	}
 	@Override
--- Magacin/src/magacin/Kucna_hemija.java
+++ Magacin/src/magacin/Kucna_hemija.java
@@ -10,6 +10,8 @@ public class Kucna_hemija extends Artikal {
 	}
 
 	public void setRok_trajanja(Date rok_trajanja) {
+		if(rok_trajanja.before(new Date()))
+			throw new RuntimeException("Rok trajanja mora biti posle danasnjeg dana");
 		this.rok_trajanja = rok_trajanja;
 	}
 
