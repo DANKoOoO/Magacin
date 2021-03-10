b1993c749983cecb8aa928edf4634d0c92527017
DANKoOoO <danko.97@hotmail.com>
2021-03-10 11:02:11 +0100

    Dodat paket magacin sa klasama: Artikal, Kancelarijski_materijal, Knjiga, Kucna_hemija. Dodati atributi sa geterima i seterima.

---
+++ Magacin/src/magacin/Kancelarijski_materijal.java
@@ -0,0 +1,5 @@
+package magacin;
+
+public class Kancelarijski_materijal extends Artikal {
+
+}
---
+++ Magacin/src/magacin/Kucna_hemija.java
@@ -0,0 +1,16 @@
+package magacin;
+
+import java.util.Date;
+
+public class Kucna_hemija extends Artikal {
+	private Date rok_trajanja;
+
+	public Date getRok_trajanja() {
+		return rok_trajanja;
+	}
+
+	public void setRok_trajanja(Date rok_trajanja) {
+		this.rok_trajanja = rok_trajanja;
+	}
+	
+}
